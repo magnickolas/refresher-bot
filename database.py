085899a867350d2ace85cf192dcfe0944ea5b9f1
Nikolai <magnickolas@gmail.com>
2020-05-14 18:31:57 +0300

    wait for the database to establish a connection on start

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 import yaml
 from motor.motor_asyncio import AsyncIOMotorClient
 from motor.motor_asyncio import AsyncIOMotorCollection
+from pymongo.errors import ServerSelectionTimeoutError
+
+from logger_creator import get_logger
+
+log = get_logger(logger_name="Database", filename="database.log")
 
 
 class DatabaseConfig:
@@ -13,6 +18,16 @@ class DatabaseConfig:
 
 class Database:
     _client = AsyncIOMotorClient(DatabaseConfig.host, DatabaseConfig.port)
+    try:
+        log.info("Connecting to database...")
+        _client.is_mongos
+    except ServerSelectionTimeoutError as ex:
+        msg = "Failed to connect to database"
+        log.exception(msg)
+        raise ex
+    else:
+        log.info("Successfully connected to database")
+
     _db = _client.get_database(DatabaseConfig.db_name)
 
     url_collection: AsyncIOMotorCollection = _db.get_collection("user_url")
